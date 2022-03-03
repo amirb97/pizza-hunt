53a6049e7f733fd6f7669f31df130db1688e8360
Era <amirb2467@gmail.com>
2022-03-02 23:37:43 -0800

    added api routes/controller for comments, and comment relationship to pizza

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes  = require('./comment-route');
 
-// add prefix of '/pizza' to routes created in 'pizza-routes.js'
+// add prefix of '/pizzas' to routes created in 'pizza-routes.js'
 router.use('/pizzas', pizzaRoutes);
+// add prefix of '/comments' to routes created in 'comment-routes.js'
+router.use('/comments', commentRoutes);
 
 module.exports = router;
